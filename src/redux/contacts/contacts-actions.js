e83afa2c2231d05fac2e1997c42a8d437eb8e043
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-10 21:40:51 +0100

    update

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,15 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 
-// export const addContact = createAction('contacts/addContact', (name, number) => {
-//   return {
-//     payload: {
-//       id: uuidv4(),
-//       name,
-//       number,
-//     },
-//   };
-// });
 export const fetchContactsRequest = createAction('contacts/fetchContactsRequest')
 export const fetchContactsSuccess = createAction('contacts/fetchContactsSuccess')
 export const fetchContactsError = createAction('contacts/fetchContactsError')
