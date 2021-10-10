b1b97c2eb4efe3c2101b7d88430f53a14e736616
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-10 21:33:23 +0100

    update

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -10,3 +10,23 @@ export const fetchContacts = () => async dispatch => {
        dispatch(contactsActions.fetchContactsError(error))
    }
 }
+
+// export const addContacts = () => async dispatch => {
+//     dispatch(contactsActions.fetchContactsRequest())
+//    try {
+//         const contacts = await contactsApi.addContact()
+//     dispatch(contactsActions.fetchContactsSuccess(contacts))
+//    } catch (error) {
+//        dispatch(contactsActions.fetchContactsError(error))
+//    }
+// }
+
+// export const fetchContacts = () => async dispatch => {
+//     dispatch(contactsActions.fetchContactsRequest())
+//    try {
+//         const contacts = await contactsApi.fetchContacts()
+//     dispatch(contactsActions.fetchContactsSuccess(contacts))
+//    } catch (error) {
+//        dispatch(contactsActions.fetchContactsError(error))
+//    }
+// }
--- src/redux/contacts/contacts-reducers.js
+++ src/redux/contacts/contacts-reducers.js
@@ -1,29 +1,23 @@
 import { createReducer } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
-import {contactsActions} from "./index";
-import initialContacts from "data/initialContacts.json";
+import { contactsActions } from "./index";
 
-const items = createReducer(initialContacts, {
-  [contactsActions.addContact]: (state, { payload }) => {
-    const isNameExist = state.find(
-      (contact) => contact.name.toLowerCase() === payload.name.toLowerCase()
-    );
-    const isNumberExist = state.find(
-      (contact) => contact.number === payload.number
-    );
-    if (isNameExist) {
-      alert(`${payload.name} is already in contacts`);
-      return state;
-    }
-    if (isNumberExist) {
-      alert(`${payload.number} is already in contacts`);
-      return state;
-    }
-    return [payload, ...state];
-  },
-  [contactsActions.deleteContact]: (state, { payload }) =>
-    state.filter((contact) => contact.id !== payload),
-});
+const items = createReducer([], {
+  [contactsActions.fetchContactsSuccess]: (_, action) => action.payload,
+  // [contactsActions.]: (_, action) => action.payload,
+  // [contactsActions.fetchContactsSuccess]: (_, action) => action.payload
+})
+
+const isLoading = createReducer(false, {
+  [contactsActions.fetchContactsRequest]: () => true,
+  [contactsActions.fetchContactsSuccess]: () => false,
+    [contactsActions.fetchContactsRequest]: () => false
+  
+})
+
+const error = createReducer(null, {
+  [contactsActions.fetchContactsError]:(_, action)=> action.payload
+})
 
 const filter = createReducer("", {
   [contactsActions.changeFilter]: (_state, { payload }) => payload,
@@ -31,5 +25,33 @@ const filter = createReducer("", {
 
 export default combineReducers({
   items,
+  isLoading,
   filter,
+  error
 });
+
+
+
+
+// const items = createReducer([], {
+//   [contactsActions.addContact]: (state, { payload }) => {
+//     const isNameExist = state.find(
+//       (contact) => contact.name.toLowerCase() === payload.name.toLowerCase()
+//     );
+//     const isNumberExist = state.find(
+//       (contact) => contact.number === payload.number
+//     );
+//     if (isNameExist) {
+//       alert(`${payload.name} is already in contacts`);
+//       return state;
+//     }
+//     if (isNumberExist) {
+//       alert(`${payload.number} is already in contacts`);
+//       return state;
+//     }
+//     return [payload, ...state];
+//   },
+//   [contactsActions.deleteContact]: (state, { payload }) =>
+//     state.filter((contact) => contact.id !== payload),
+// });
+
