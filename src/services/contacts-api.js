8a0f38fb7ac9d40071396634f026b754b5ceebba
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-11 23:02:20 +0100

    update

--- src/services/contacts-api.js
+++ src/services/contacts-api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = '';
 
 export async function fetchContacts() {
     const { data } = await axios.get('/contacts')
