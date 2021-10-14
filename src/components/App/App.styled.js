4cf31ff8a311ddc5a4a4046ea85cc8f32697e072
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-14 22:12:50 +0100

    update

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,13 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  margin: 20px;
-
-  background: linear-gradient(45deg, rgb(160, 239, 240), rgb(8, 48, 140));
+   margin: 20px;
+  background: linear-gradient(45deg, rgb(240, 160, 199), rgb(110,10,172));
   padding: 10px;
   width: 700px;
-  box-shadow: 5px 6px 9px rgb(11, 93, 194);
-  border-radius: 8px;
+  box-shadow: 5px 6px 9px rgb(58,6,90);
+  border-radius: 5px;
   text-align: center;
 `;
 export const TitleH1 = styled.h1`
