678bb205da6ef89fb466d162728dcfbe89edff75
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-15 20:27:21 +0100

    update

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -14,11 +14,11 @@ const styles = {
 };
 
 export default function AppBar() {
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+//   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return (
     <header style={styles.header}>
       <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      {authSelectors.getIsLoggedIn ? <UserMenu /> : <AuthNav />}
     </header>
   );
 }
