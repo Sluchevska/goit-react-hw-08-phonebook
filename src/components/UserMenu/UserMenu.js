aa6fe0900e76d06cb46913f75dd87c2a2327e776
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-24 22:12:07 +0100

    update

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,18 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { authSelectors, authOperations } from '../../redux/auth';
-import { Container, NameStyle, AvatarStyle } from './UserMenu.styled';
-// import defaultAvatar from './default-avatar.png';
-
-
+import { Container, NameStyle } from './UserMenu.styled';
 
 export default function UserMenu() {
   const dispatch = useDispatch();
   const name = useSelector(authSelectors.getUsername);
-//   const avatar = defaultAvatar;
 
   return (
     <Container>
-      {/* <AvatarStyle src={avatar} alt="" width="32"  /> */}
       <NameStyle>Welcome, {name}</NameStyle>
       <button type="button" onClick={() => dispatch(authOperations.logOut())}>
         Sign out
