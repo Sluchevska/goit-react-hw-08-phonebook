1d2d218345f22100bdde3c5f5c3987074b28eedb
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-24 14:27:52 +0100

    update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
-
 import { Container } from './App.styled';
-import AppBar from '../AppBar/AppBar'
+import AppBar from '../AppBar/AppBar';
 import { Route, Switch } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { lazy, Suspense, useEffect } from 'react';
@@ -8,41 +7,46 @@ import { authOperations, authSelectors } from 'redux/auth';
 import PublicRoute from 'components/PublicRouter';
 import PrivateRoute from 'components/PrivateRoute';
 
-const HomeView = lazy(()=> import('../../views/HomeView/HomeView.js'))
-const RegisterView = lazy(() => import('../../views/RegisterView/RegisterView'))
-const ContactsView = lazy(() => import('../../views/ContactsView/ContactsView.js'))
-const LoginView = lazy(()=> import('../../views/LoginView/LoginView.js'))
+const HomeView = lazy(() => import('../../views/HomeView/HomeView.js'));
+const RegisterView = lazy(() =>
+  import('../../views/RegisterView/RegisterView'),
+);
+const ContactsView = lazy(() =>
+  import('../../views/ContactsView/ContactsView.js'),
+);
+const LoginView = lazy(() => import('../../views/LoginView/LoginView.js'));
 
 export default function App() {
   const dispatch = useDispatch();
-   const isRefreshingCurrentUser = useSelector(authSelectors.getIsRefreshingCurrentUser);
+  const isRefreshingCurrentUser = useSelector(
+    authSelectors.getIsRefreshingCurrentUser,
+  );
 
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
   }, [dispatch]);
-  
+
   return (
-     !isRefreshingCurrentUser && (
-    <Container>
-      <AppBar />
+    !isRefreshingCurrentUser && (
+      <Container>
+        <AppBar />
         <Switch>
-           <Suspense fallback={<p>Loading...</p>}>
-         <PublicRoute exact path="/">
-                <HomeView />
-              </PublicRoute>
-    <PublicRoute exact path="/register" restricted>
-                <RegisterView />
-              </PublicRoute>
-              <PublicRoute exact path="/login" redirectTo="/contacts" restricted>
-                <LoginView />
+          <Suspense fallback={<p>Loading...</p>}>
+            <PublicRoute exact path="/">
+              <HomeView />
             </PublicRoute>
-             <PrivateRoute path="/contacts" redirectTo="/login">
-                <ContactsView />
-              </PrivateRoute>
-       
-         </Suspense> 
-      </Switch>
-     
-    </Container>)
+            <PublicRoute exact path="/register" restricted>
+              <RegisterView />
+            </PublicRoute>
+            <PublicRoute exact path="/login" redirectTo="/contacts" restricted>
+              <LoginView />
+            </PublicRoute>
+            <PrivateRoute path="/contacts" redirectTo="/login">
+              <ContactsView />
+            </PrivateRoute>
+          </Suspense>
+        </Switch>
+      </Container>
+    )
   );
 }
