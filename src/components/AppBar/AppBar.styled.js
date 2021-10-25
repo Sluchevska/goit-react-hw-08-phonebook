706f0b2d3a02b35111d057c12350e60a61b72233
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-25 16:59:23 +0100

    update

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -5,4 +5,14 @@ display: flex;
     justify-content: space-between;
     align-items: center;
     border-bottom: 1px solid #2A363B;
+    background-image: linear-gradient(
+         rgba(86, 86, 218, 0.7),
+      rgba(230, 230, 247, 0.7)
+     
+    );
+
+    padding: 15px 5px;
+    box-shadow: 4px 5px 8px rgb(34, 49, 190);
+ border-radius: 5px;
+
 `
--- src/views/HomeView/HomeView.styled.js
+++ src/views/HomeView/HomeView.styled.js
@@ -1,23 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-//  min-height: calc(100vh - 50px);
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//      background-image: linear-gradient(
-//       rgba(247, 247, 248, 0.4),
-//       rgba(47, 48, 58, 0.4)
-//     ),
-//     url(../../img/logoPHB.jpg);
-//      max-width: 1600px;
-
-//   background-repeat: no-repeat;
-//   background-position: center;
+padding-top:25px;
 `
 
 export const Title = styled.h1`
  font-weight: 500;
     font-size: 48px;
     text-align: center;
+    
 `
