ad135de1d379a9896931f7a2ab63c87bb357875e
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-25 21:54:47 +0100

    update

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,11 +3,11 @@ import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { authSelectors } from 'redux/auth';
 import { NavigationStyle, Span } from '../AuthNav/AuthNav.styled';
-import { BsFillPeopleFill, BsFillHouseDoorFill} from "react-icons/bs";
+import { BsFillPeopleFill, BsFillHouseDoorFill } from 'react-icons/bs';
 
 const styles = {
   link: {
-    color:'#383764',
+    color: '#383764',
   },
   activeLink: {
     color: '#100de0',
@@ -19,30 +19,33 @@ const Navigation = () => {
   return (
     <nav>
       <NavigationStyle>
-        <NavLink to="/" exact activeStyle={styles.activeLink}  style={styles.link}>
-          <BsFillHouseDoorFill />
-          <Span>
-          Home
-          </Span>
-        </NavLink>
+        {!isLoggedIn && (
+          <NavLink
+            to="/"
+            exact
+            activeStyle={styles.activeLink}
+            style={styles.link}
+          >
+            <BsFillHouseDoorFill />
+            <Span>Home</Span>
+          </NavLink>
+        )}
       </NavigationStyle>
       <NavigationStyle>
         {isLoggedIn && (
           <NavLink
             to="/contacts"
             exact
-      style={styles.link}
+            style={styles.link}
             activeStyle={styles.activeLink}
           >
             <BsFillPeopleFill />
-            <Span>
-            Contacts
-            </Span>
+            <Span>Contacts</Span>
           </NavLink>
         )}
       </NavigationStyle>
     </nav>
   );
-}
+};
 
 export default Navigation;
